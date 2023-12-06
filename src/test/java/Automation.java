a198d428ebc13dbb4a573864869ae18527a2fa38
Gabriel <fullstackgabe@gmail.com>
2023-12-06 15:24:49 -0300

    xpath, sendKeys, maximize window

--- src/test/java/Automation.java
+++ src/test/java/Automation.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Automation {
@@ -8,8 +10,9 @@ public class Automation {
         System.setProperty("webdriver.chrome.driver", "src\\drive\\chromedriver.exe");
         WebDriver driver  = new ChromeDriver();
 
-        driver.get("https://google.com");
-
-        driver.quit();
+        driver.get("http://sistema.novagne.com.br/novagne/");
+        driver.manage().window().maximize();
+        WebElement element = driver.findElement(By.xpath("/html/body/table/tbody/tr[2]/td[1]/form/table/tbody/tr[1]/td/font/font/input[1]"));
+        element.sendKeys("gabriel");
     }
 }

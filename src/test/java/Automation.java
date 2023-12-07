0b0d52e8b386aa03d2b8d33d7764cff5145386e2
Gabriel <fullstackgabe@gmail.com>
2023-12-07 16:43:26 -0300

    add comments

--- src/test/java/Automation.java
+++ src/test/java/Automation.java
@@ -12,27 +12,33 @@ import org.openqa.selenium.Keys;
 public class Automation {
     @Test
     public void pesquisarGoogle() {
+
         System.setProperty("webdriver.chrome.driver", "src\\drive\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
+        //open & maximize google-chrome
         driver.get("https://www.kaggle.com/datasets");
         driver.manage().window().maximize();
 
         waitAndHandleException();
 
+        //capture element DOM & search
         WebElement element = driver.findElement(By.xpath("//*[@id=\"site-content\"]/div[2]/div[4]/div/div/div[1]/div/input"));
         element.sendKeys("nintendo 64 games");
         element.sendKeys(Keys.ENTER);
 
         waitAndHandleException();
 
+        //click on the element
         WebElement table = driver.findElement(By.xpath("//*[@id=\"site-content\"]/div[2]/div[5]/div/div/div/ul/li[1]/div[1]/a/div[2]/div"));
         table.click();
 
         waitAndHandleException();
 
+        //add data to a list
         List<WebElement> datasets = driver.findElements(By.cssSelector(".sc-cCYyox, .sc-cjERFW, .jxeAHX, .fWvmCS"));
 
+        //capture the datasets and write to txt file
         try (FileWriter writer = new FileWriter("output.txt", true)) {
             for (WebElement dataset : datasets) {
                 String text = dataset.getText();
@@ -44,6 +50,7 @@ public class Automation {
         }
     }
 
+        //standby method
         public void waitAndHandleException() {
         try {
             Thread.sleep(3000);

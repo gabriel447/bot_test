3f666c3dfdc88dcf5921c6da810773b81ba0dfca
Gabriel <fullstackgabe@gmail.com>
2023-12-07 16:22:51 -0300

    alter project, web scrapping data, save txt data

--- src/test/java/Automation.java
+++ src/test/java/Automation.java
@@ -4,15 +4,51 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import org.openqa.selenium.Keys;
+
 public class Automation {
     @Test
     public void pesquisarGoogle() {
         System.setProperty("webdriver.chrome.driver", "src\\drive\\chromedriver.exe");
-        WebDriver driver  = new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
 
-        driver.get("http://sistema.novagne.com.br/novagne/");
+        driver.get("https://www.kaggle.com/datasets");
         driver.manage().window().maximize();
-        WebElement element = driver.findElement(By.xpath("/html/body/table/tbody/tr[2]/td[1]/form/table/tbody/tr[1]/td/font/font/input[1]"));
-        element.sendKeys("gabriel");
+
+        waitAndHandleException();
+
+        WebElement element = driver.findElement(By.xpath("//*[@id=\"site-content\"]/div[2]/div[4]/div/div/div[1]/div/input"));
+        element.sendKeys("nintendo 64 games");
+        element.sendKeys(Keys.ENTER);
+
+        waitAndHandleException();
+
+        WebElement games = driver.findElement(By.xpath("//*[@id=\"site-content\"]/div[2]/div[5]/div/div/div/ul/li[1]/div[1]/a/div[2]/div"));
+        games.click();
+
+        waitAndHandleException();
+
+        List<WebElement> dados = driver.findElements(By.cssSelector("sc-cCYyox, .sc-cjERFW, .jxeAHX, .fWvmCS"));
+
+        try (FileWriter writer = new FileWriter("output.txt", true)) {
+            for (WebElement dado : dados) {
+                String text = dado.getText();
+                /*System.out.println(text);*/
+                writer.write(text + System.lineSeparator());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+        public void waitAndHandleException() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
